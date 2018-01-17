1c906e7c0eea18433919bee8f1c4e16f48537c9f
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-17 14:08:02 +0530

    initial commit

---
+++ Hiru_M2_kinesis170102/Hiru_M2_kinesis170102-lambda.js
@@ -0,0 +1,24 @@
+let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+exports.handler = function (event, context, callback) {
+
+
+
+	kinesis.describeStream({
+		StreamName: 'Hiru_Stream'
+	}).promise()
+		.then(describeStreamData => {
+			console.log('Data',describeStreamData);
+			callback(null, describeStreamData);
+			
+		})
+		.catch(err => {
+			console.log('Error',err);
+			callback(null, err);
+			
+		});
+
+
+
+	callback(null, 'Successfully executed');
+}
